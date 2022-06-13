4bdaa6d5973c72e1d1bc086fbbddf5069f11a1c8
Lauren Metz <Laurenmetz15@gmail.com>
2022-06-12 20:06:52 -0400

    commit tree 739174904e6ee4c0a35de3d8f757c4c1a557d38c

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(name) {
+    bestCustomer = 'not bob';
+    return bestCustomer;
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+    return bestCustomer;
+}
+
+const leastFavoriteCustomer = 'bobbette'
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'bobby';
+}
